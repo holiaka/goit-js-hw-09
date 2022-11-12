c8d2b0ad983950c2a549e7e6960c7368c64a0915
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2022-11-13 00:41:32 +0200

    First ver for task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+// HTML links
+ref = {
+  body: document.querySelector('body'),
+  startButton: document.querySelector('button[data-start]'),
+  stopButton: document.querySelector('button[data-stop]'),
+};
 
+ref.startButton.addEventListener('click', onStartButtonClick);
+ref.stopButton.addEventListener('click', onStopButtonClick);
+
+ref.stopButton.setAttribute('disabled', '');
+
+let intervalId;
+
+function onStartButtonClick() {
+  ref.stopButton.removeAttribute('disabled');
+  ref.startButton.setAttribute('disabled', '');
+  intervalId = setInterval(changeBodyColor, 1000);
+}
+
+function onStopButtonClick() {
+  ref.startButton.removeAttribute('disabled');
+  ref.stopButton.setAttribute('disabled', '');
+  clearInterval(intervalId);
+}
+
+function changeBodyColor() {
+  ref.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
