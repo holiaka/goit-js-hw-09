226ab356aac98735dc6bdf220ccd60c807220790
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2022-11-14 19:41:26 +0200

    Control

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,13 +5,15 @@ ref = {
   stopButton: document.querySelector('button[data-stop]'),
 };
 
+// Add Event Listener
 ref.startButton.addEventListener('click', onStartButtonClick);
 ref.stopButton.addEventListener('click', onStopButtonClick);
 
+// Support code
 ref.stopButton.setAttribute('disabled', '');
-
 let intervalId;
 
+// Functions
 function onStartButtonClick() {
   ref.stopButton.removeAttribute('disabled');
   ref.startButton.setAttribute('disabled', '');
@@ -31,3 +33,20 @@ function changeBodyColor() {
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// class Colors {
+//   // Синтаксис объявления метода класса
+//   constructor(ref) {
+//     // Инициализация свойств экземпляра
+//     this.body = ref.body;
+//     this.startButtton = ref.startButtton;
+//   }
+
+//   create() {
+//     console.log(body)
+//   }
+
+//   get body() {
+//     return this.body;
+//   }
+// }
