42bf45d22a1ddba8f5338f06b9eabee8845b32af
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2022-11-15 20:05:17 +0200

    Final

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,13 @@
+// Load libraries
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Link on FORM
 const formButton = document.querySelector("form");
 
+// Her listeber
 formButton.addEventListener("submit", onButtonClick);
 
-
+// Func. on click 
 function onButtonClick(evt) {
   evt.preventDefault();
 
@@ -13,47 +18,38 @@ function onButtonClick(evt) {
   console.log(typeof amount.value);
   
   if ((delayT < 0) || (stepT < 0) || (amountT <= 0)) {
-    alert("You enter no corect values")
+    Notify.failure("You enter no corect values");
   } else {
     for (let i = 1; i <= amountT + 1; i++) {
       const fullDelay = delayT + ((i - 1) * stepT);
       console.log(fullDelay);
-      createPromise(i, fullDelay);
+      createPromise(i, fullDelay).then(successCallback).catch(failureCallback);
     }
-  }
-  
+    Notify.success("It`s Ok!!!");
+  }  
 }
 
+// Func. with Promise
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   
-  if (shouldResolve) {
-    // Fulfill
-    console.log('Fulfill');
-  } else {
-    // Reject
-    console.log('Reject');
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {      
+
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
 
+// Callback func.
+function successCallback({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
 
-const fetchUserFromServer = username => {
-  return new Promise((resolve, reject) => {
-    console.log(`Fetching data for ${username}`);
-
-    setTimeout(() => {
-      // Change value of isSuccess variable to simulate request status
-      const isSuccess = true;
-
-      if (isSuccess) {
-        resolve("success value");
-      } else {
-        reject("error");
-      }
-    }, 2000);
-  });
-};
-
-fetchUserFromServer("Mango")
-  .then(user => console.log(user))
-  .catch(error => console.error(error));
+function failureCallback({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+}
