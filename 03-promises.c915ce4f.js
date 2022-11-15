487d1c1e6ba1892c7dc7a710214caf8972e5257d
holiaka <46654821+holiaka@users.noreply.github.com>
2022-11-15 16:39:33 +0000

    Deploying to gh-pages from @ holiaka/goit-js-hw-09@2f03cec65b71af9216504dc07606107ce01132d4 🚀

--- 03-promises.c915ce4f.js
+++
@@ -1,2 +0,0 @@
-setInterval(console.log("my"),500);
-//# sourceMappingURL=03-promises.c915ce4f.js.map
--- 03-promises.f27403b0.js
+++
@@ -1,2 +0,0 @@
-let e;e=setInterval(console.log("my"),500);
-//# sourceMappingURL=03-promises.f27403b0.js.map
